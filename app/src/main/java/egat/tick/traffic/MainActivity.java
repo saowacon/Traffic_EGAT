4206b4bfa9acba7a05812a2dc98fd478ebda4830
saowacon <saowacon.j@egat.co.th>
2015-08-26 10:54:02 -0700

    ListView Done

--- app/src/main/java/egat/tick/traffic/MainActivity.java
+++ app/src/main/java/egat/tick/traffic/MainActivity.java
@@ -22,19 +22,60 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Bind Widget
+        // Bind Widget เชื่อมตัวแปรกับ Widget บน GUI
         bindWidget();
 
-        //Button Controller
+        //Button Controller ทำให้ปุ่มคลิกได้
         buttonController();
+
+        //Create ListView
+        createListView();
+
     } // Main Method
+
+    private void createListView() {
+        // Setup intDrawable
+        int intDrawable[] = {R.drawable.traffic_01, R.drawable.traffic_02,
+                R.drawable.traffic_03, R.drawable.traffic_04, R.drawable.traffic_05,
+                R.drawable.traffic_06, R.drawable.traffic_07, R.drawable.traffic_08,
+                R.drawable.traffic_09, R.drawable.traffic_10, R.drawable.traffic_11,
+                R.drawable.traffic_12, R.drawable.traffic_13, R.drawable.traffic_14,
+                R.drawable.traffic_15, R.drawable.traffic_16, R.drawable.traffic_17,
+                R.drawable.traffic_18, R.drawable.traffic_19, R.drawable.traffic_20};
+        // Setup strTitle ใช้ Array เริ่มต้นที่ 0
+        String strTitle[] = new String[20];
+        strTitle[0] = "ห้ามเลี้ยวซ้าย";
+        strTitle[1] = "ห้ามเลี้ยวขวา";
+        strTitle[2] = "ตรงไป";
+        strTitle[3] = "เลี้ยวขวา";
+        strTitle[4] = "เลี้ยวซ้าย";
+        strTitle[5] = "ออก";
+        strTitle[6] = "เข้า";
+        strTitle[7] = "ออก";
+        strTitle[8] = "หยุด";
+        strTitle[9] = "จำกัดความสูง";
+        strTitle[10] = "ทางแยก";
+        strTitle[11] = "ห้ามกลับรถ";
+        strTitle[12] = "ห้ามจอด";
+        strTitle[13] = "รถสวน";
+        strTitle[14] = "ห้ามแซง";
+        strTitle[15] = "เข้า";
+        strTitle[16] = "หยุดตรวจ";
+        strTitle[17] = "จำกัดความเร็ว";
+        strTitle[18] = "จำกัดความกว้าง";
+        strTitle[19] = "จำกัดความสูง";
+
+        MyAdapter objMyAdapter = new MyAdapter(MainActivity.this, intDrawable, strTitle);
+        trafficListView.setAdapter(objMyAdapter);
+
+    } // Create ListView
 
     private void buttonController() {
         aboutMeButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 // Sound Effect เมื่อคลิกที่ปุ่ม
-                MediaPlayer buttonMediaPlayer = MediaPlayer.create(getBaseContext(),R.raw.effect_btn_long);
+                MediaPlayer buttonMediaPlayer = MediaPlayer.create(getBaseContext(), R.raw.effect_btn_long);
                 buttonMediaPlayer.start();
 
                 // Intent to WebView
